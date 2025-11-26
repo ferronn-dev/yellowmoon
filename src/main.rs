9863303c8a6d042d972c67952c1328442e7db7d9
Ferronn <ferronn@ferronn.dev>
2025-11-26 11:52:50 -0600

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,14 @@ use clap::Parser;
 
 #[derive(Parser)]
 struct Cli {
-  filename: String,
+    filename: String,
 }
 
 fn main() -> Result<(), anyhow::Error> {
-  let cli = Cli::parse();
-  println!("{:#?}", yellowmoon::undump::undump(&std::fs::read(cli.filename)?)?);
-  Ok(())
+    let cli = Cli::parse();
+    println!(
+        "{:#?}",
+        yellowmoon::undump::undump(&std::fs::read(cli.filename)?)?
+    );
+    Ok(())
 }
